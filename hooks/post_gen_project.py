fdbc4cca03c3039ac9ff16c06edbeffad2a3dc3c
WardDeb <ward@deboutte.be>
2025-09-26 13:38:10 +0200

    pixi in all projects, update nf with barebone structure.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,13 +1,12 @@
 def main():
     print("Cookiecutter done. Setup git and push to github:")
     print("\n")
-    print(f"cd {{ cookiecutter.project_slug }}")
-    print(f"git init")
-    print(f"git checkout -b main")
-    print(f"git remote add origin git@github.com:{{ cookiecutter.full_name }}/{{ cookiecutter.project_slug }}.git")
-    print("git add .")
-    print("git commit -m 'init repo'")
-    print("git push -u origin main")
+    print(f"git -C {{ cookiecutter.project_slug }} init")
+    print(f"git -C {{ cookiecutter.project_slug }} checkout -b main")
+    print(f"git -C {{ cookiecutter.project_slug }} add .")
+    print(f"git -C {{ cookiecutter.project_slug }} commit -m 'cookiecutter pudding - initial commit'")
+    print(f"git -C {{ cookiecutter.project_slug }} remote add origin git@github.com:{{ cookiecutter.full_name }}/{{ cookiecutter.project_slug }}.git")
+    print(f"git -C {{ cookiecutter.project_slug }} push -u origin main")
 
 if __name__ == "__main__":
     main()
---
+++ nfproject/{{ cookiecutter.project_slug }}/README.md
@@ -0,0 +1,4 @@
+# {{ cookiecutter.project_name }}
+
+Initial repository for {{ cookiecutter.project_name }}.
+
